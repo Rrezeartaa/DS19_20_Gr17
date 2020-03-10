9dcca94c3f0da9e4d28759b45d6e72b340cbbce9
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-03-10 16:39:35 +0100

    Update ds.cs

--- ds.cs
+++ ds.cs
@@ -127,6 +127,16 @@ namespace ds
 
 		}
 
+		//public static void show(string text, int rails)
+		//{
+			//for (int i = 0; i < text.Length; i++)
+			//{
+				
+			//		Console.WriteLine(text[i]);
+				
+			//}
+			
+		//}
 	}
 
 }
