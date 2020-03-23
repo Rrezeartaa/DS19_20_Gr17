661770f02f402979e91732b94f1ac8d76c7379b5
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-03-23 22:19:22 +0100

    Update Count.cs

--- Count.cs
+++ Count.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +12,19 @@ namespace ConsoleApp12
     {
         public static void Main()
         {
+            string str1;
+            
+            int j, fja, e ;
             string str;
             int i, wrd, l;
-            //numerimi i fjalive 
 
-            Console.Write("\n\nJepe nje fjali :\n");
+            Console.Write("\n\nShkruaj fjal apo fjali :\n");
             Console.Write("------------------------------------------------------\n");
             Console.Write("Input the string : ");
             str = Console.ReadLine();
 
             l = 0;
-            wrd = 1;
+            wrd = 0;
 
           
             while (l <= str.Length - 1)
@@ -30,11 +34,35 @@ namespace ConsoleApp12
                 {
                     wrd++;
                 }
-
+                
                 l++;
             }
+            
+            e = 0;
+            fja = 1;
+
+            while (e <= str.Length - 1)
+            {
+
+                if (str[e] == '.' || str[e] == '\n' || str[e] == '\t')
+                {
+                    fja++;
+                }
+
+                e++;
+            }
+
+
+
+
+
+
+
+
+
 
             Console.Write("Numri i fjalive te dhena : {0}\n", wrd);
+            Console.Write("Numri i fjalive te dhena : {0}\n", fja);
         }
     }
 }
