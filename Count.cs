73d5c84ad63538e0826390095b54d1246b9ddead
vanesarama <56617704+vanesarama@users.noreply.github.com>
2020-03-27 00:36:16 +0100

    Update Count.cs

--- Count.cs
+++ Count.cs
@@ -40,7 +40,7 @@ namespace ds
             }
             return word;
         }
-            public static int countletters(String text)
+        public static int countletters(String text)
         {
             int nriShkronjave = 0;
 
@@ -54,6 +54,95 @@ namespace ds
             }
             return nriShkronjave;
         }
-        
+
+        public static int countcharacters(String text)
+        {
+            int nrKaraktereve = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                    nrKaraktereve++;
+            }
+
+            return nrKaraktereve;
+        }
+
+        public static int countsymbols(String text)
+        {
+            int nrSimboleve = 0;
+
+            text = text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ((text[i] >= 'a' && text[i] <= 'z') || text[i] == ' '
+                        || (text[i] >= '0' && text[i] <= '9'))
+                {
+                    continue;
+                }
+                else
+                {
+                    nrSimboleve++;
+                }
+            }
+            return nrSimboleve;
+        }
+
+        public static int countvowels(String text)
+        {
+            int nrZanoreve = 0;
+            text = text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                switch (text[i])
+                {
+                    case 'a':
+                        nrZanoreve++;
+                        break;
+                    case 'e':
+                        nrZanoreve++;
+                        break;
+                    case 'o':
+                        nrZanoreve++;
+                        break;
+                    case 'u':
+                        nrZanoreve++;
+                        break;
+                    case 'i':
+                        nrZanoreve++;
+                        break;
+                }
+            }
+            return nrZanoreve;
+        }
+
+        public static int countspaces(String text)
+        {
+            int numriHaps = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                    numriHaps++;
+            }
+            return numriHaps;
+        }
+
+        public static int countconsonants(String text)
+        {
+            return text.Length - countspaces(text) - countvowels(text) - countsymbols(text);
+        }
+public static int countsent(String strin)
+        {
+            char[] chars = strin.ToLower().ToCharArray();
+            int sent=0;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '.' || chars[i] == '?' || chars[i] == '!')
+                {
+                    sent++;
+                }
+            }
+            return sent;
+        }
     }
 }
+
