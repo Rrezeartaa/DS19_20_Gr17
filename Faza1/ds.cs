ef8ea786751607a41e5514eb0841948c82f5c96d
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-03-28 22:07:21 +0100

    Update ds.cs

--- Faza1/ds.cs
+++ Faza1/ds.cs
@@ -11,8 +11,6 @@ namespace ds
     {
         public static void Main(string[] args)
         {
-
-
             if (args.Length < 3)
             {
                 throw new IndexOutOfRangeException("\n\tNuk ka argumente te mjaftueshme!" +
@@ -21,9 +19,7 @@ namespace ds
             else if (args.Length > 5)
             {
                 throw new IndexOutOfRangeException("\nNumri i argumenteve eshte i tepert!");
-            }
-            
-            
+            }            
             else if ("count".Equals(args[0]))
             {
                 if ("lines".Equals(args[1]))
@@ -56,19 +52,16 @@ namespace ds
                 {
                     Console.WriteLine(count.countsent(args[2]));
                 }
-
                 else
                 {
-                    Console.WriteLine("\nArgumentet jane jovalide! \nSigurohuni qe keni shkruar argumentet si ne vijim:  \n" +
+                    Console.WriteLine("\nArgumentet jane jovalide! \nSigurohuni qe keni shkruar argumentet si ne vijim: \n" +
                             "ds count lines <text>: per te llogaritur numrin e rreshtave\n" +
                             "ds count words <text>: per te llogaritur numrin e fjaleve\n" +
                             "ds count letters <text>: per te llogaritur numrin e shkronjave\n" +
                             "ds count symbols <text>: per te llogaritur numrin e simboleve\n" +
                             "ds count vowels <text>: per te llogaritur numrin e zanoreve\n" +
                             "ds count consonants <text>: per te llogaritur numrin e bashketingelloreve\n"+
-                            "ds count sentences <text>: per te llogaritur numrin e fjalive\n"
-                            );
-
+                            "ds count sentences <text>: per te llogaritur numrin e fjalive\n");
                 }
             }
              else if ("numerical".Equals(args[0]))
@@ -77,16 +70,12 @@ namespace ds
                 {
                     if ("--separator".Equals(args[2]))
                     {
-
                         string result = Numerical.Encode(args[4]);
                         string resultt = Numerical.separator(args[3], args[4]);
                         Console.WriteLine(result);
                         Console.WriteLine(resultt);
                         Console.ReadKey();
-
-
                     }
-
                     else
                     {
                         string text = args[2];
@@ -94,18 +83,12 @@ namespace ds
                         Console.WriteLine(result);
                         Console.ReadKey();
                     }
-
                 }
                 else if ("decode".Equals(args[1]))
                 {
-
-
                     string code = args[2];
-
                     Numerical.Decode(code);
-
                     Console.ReadKey();
-
                 }
               else
               {
@@ -113,137 +96,51 @@ namespace ds
                             "ds numerical encode <text>: per ta enkoduar tekstin ne pozitat alfabetike te shkronjave.\n" +
                             "ds numerical decode <code>: per ta dekoduar vargun <code> nga shifrat ne shkronjat perkatese.\n" +
                             "ds numerical encode --separator <char> <text>: per ta paraqitur tekstin e enkoduar te ndare me ane"+
-                                  " te karakterit te specifikuar.\n"
-                                 
-                            );
-              
+                            " te karakterit te specifikuar.\n");              
               }
 
             }
-            
+               
             else if ("rail-fence".Equals(args[0]))
             {
                 if ("encrypt".Equals(args[1]))
                 {
-
                     if ("--show".Equals(args[2]))
                     {
-                        string plaintext = args[4];
-                        string result = Railfence.encrypt(plaintext, int.Parse(args[3]));
-                        Console.WriteLine(result); 
-                        Railfence.show(plaintext, int.Parse(args[3]));Console.ReadKey();
+                        string plaintext = args[4];  
+                        railfence.show(plaintext, int.Parse(args[3]));
+                        Console.ReadKey();
                     }
                     else
                     {
                         string plaintext = args[3];
-                        string result = Railfence.encrypt(plaintext, int.Parse(args[2]));
-                        Console.WriteLine(result);
+                        railfence1.encrypt(plaintext, int.Parse(args[2]));                       
                         Console.ReadKey();
                     }
 
                 }
                 else if ("decrypt".Equals(args[1]))
-                {
-                    if ("show".Equals(args[2]))
-                    {
-                        string ciphertext = args[4];
-                        string result = Railfence.decrypt(ciphertext, int.Parse(args[3]));
-                        Console.WriteLine(result); Console.ReadKey();
-                        Railfence.show(ciphertext, int.Parse(args[3]));
-                    }
-                    else
-                    {
+                {                 
                         string ciphertext = args[3];
-                        string result = Railfence.decrypt(ciphertext, int.Parse(args[2]));
+                        string result = railfence.decrypt(ciphertext, int.Parse(args[2]));
                         Console.WriteLine(result);
-                        Console.ReadKey();
-                    }
+                        Console.ReadKey();   
                 }
                 else
                 {
                     Console.WriteLine("\nArgumentet jane jovalide! \nSigurohuni qe keni shkruar argumentet si ne vijim:  \n" +
                             "ds rail-fence encrypt <rails> <plaintext>: per enkriptimin e plaintext-it\n" +
                             "ds rail-fence decrypt <rails> <ciphertext>: per dekriptimin e ciphertext-it\n" +
-                            "ds rail-fence encrypt --show <rails> <plaintext>: per te pare tekstin e enkriptuar te organizuar\n" +
-                            "ds rail-fence decrypt --show <rails> <ciphertext>: per te pare tekstin e dekriptuar te organizuar\n");
-
+                            "ds rail-fence encrypt --show <rails> <text>: per te pare tekstin e enkriptuar te organizuar ne shirita\n");
                 }
-                
-
             }
-            
-            else if ("rail-fence1".Equals(args[0]))
-            {
-                if ("encrypt".Equals(args[1]))
-                {
-
-                    if ("--show".Equals(args[2]))
-                    {
-                        string plaintext = args[4];
-                        
-                        railfence1.encrypt1(plaintext, int.Parse(args[3]));
-                        railfence1.show1(plaintext, int.Parse(args[3]));
-
-                        Console.ReadKey();
-                    }
-                    else
-                    {
-                        string plaintext = args[3];
-                       railfence1.encrypt1(plaintext, int.Parse(args[2]));
-                        
-                        Console.ReadKey();
-                    }
-
-                }
-                else if ("decrypt".Equals(args[1]))
-                {
-                    if ("show".Equals(args[2]))
-                    {
-                        string ciphertext = args[4];
-                        string result= railfence1.decrypt1(ciphertext, int.Parse(args[3]));
-                        Console.WriteLine(result);
-                        railfence1.show1(ciphertext, int.Parse(args[3]));
-                      Console.ReadKey();
-                      
-                    }
-                    else
-                    {
-                        string ciphertext = args[3];
-                        string result = railfence1.decrypt1(ciphertext, int.Parse(args[2]));
-                        Console.WriteLine(result);
-
-                        Console.ReadKey();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("\nArgumentet jane jovalide! \nSigurohuni qe keni shkruar argumentet si ne vijim:  \n" +
-                            "ds rail-fence1 encrypt <rails> <text>: per enkriptimin e plaintext-it\n" +
-                            "ds rail-fence1 decrypt <rails> <text>: per dekriptimin e ciphertext-it\n" +
-                            "ds rail-fence1 encrypt --show <rails> <text>: per te pare tekstin e enkriptuar te organizuar\n" +
-                            "ds rail-fence1 decrypt --show <rails> <text>: per te pare tekstin e dekriptuar te organizuar\n" 
-                            
-                            );
-
-                }
-
-            }
-            
             else
             {
                 Console.WriteLine("\nArgumentet jane jovalide. Argumenti i pare eshte njeri prej funksioneve tona: \n" +
                         " count\n" +
                         " numerical\n" +
-                        " rail-fence\n"+
-                                  "rail-fence1\n\n");
-                
+                        " rail-fence\n\n");
             }
-
-
-
         }
-
-
-        
     }
 }
