96969209699fbd68c3c876be2ce1365104455ede
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-04-14 22:36:31 +0200

    Komanda create-user

---
+++ Faza2(metodat)/rsa.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace ds
+{
+    class rsa
+    {
+        private static RSAParameters publicKey;
+        private static RSAParameters privateKey;
+        static string CONTAINER_NAME = "MyGeneratedKeys";
+        public enum KeySizes { 
+           SIZE_512=512,
+           SIZE_1024=1024,
+            SIZE_2048=2048,
+            SIZE_952 = 952,
+            SIZE_1369 = 1369
+
+        };
+        public static void GenKey(string name)
+        {
+            string publicKeyFile = "keys/" + name + ".pub.xml";
+            string privateKeyFile= "keys/" + name + ".xml";
+            using (var rsa = new RSACryptoServiceProvider((int)KeySizes.SIZE_2048))
+            {
+                if (!File.Exists(publicKeyFile) && !File.Exists(privateKeyFile))
+                {
+                    rsa.PersistKeyInCsp = false;
+                    if (File.Exists(privateKeyFile))
+                        File.Delete(privateKeyFile);
+                    if (File.Exists(publicKeyFile))
+                        File.Delete(publicKeyFile);
+                    string publicKey = rsa.ToXmlString(false);
+                    File.WriteAllText(publicKeyFile, publicKey);
+                    string privateKey = rsa.ToXmlString(true);
+                    File.WriteAllText(privateKeyFile, privateKey);
+
+                    Console.WriteLine("Eshte krijuar celesi privat '" + publicKeyFile + "'");
+                    Console.WriteLine("Eshte krijuar celesi publik '" + privateKeyFile + "'");
+                }
+                else
+                    Console.WriteLine("Gabim: Celesi '" + name + "' ekziston paraprakisht.");
+            }
+      
+        }
+
+     
+
+    }
+}
