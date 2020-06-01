98bc75a05b88c936ce453955df58f2b83f84ed74
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-06-01 16:14:38 +0200

    Update rsa.cs

--- Faza2(metodat)/rsa.cs
+++ Faza2(metodat)/rsa.cs
@@ -31,10 +31,53 @@ namespace ds
             string strRegexi = @"^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{8,}$";
             Regex ree = new Regex(strRegexi);
             Console.WriteLine("Jepni fjalekalimin:");
-            string password;
-            password= Console.ReadLine();
+            string password = "";
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                    {
+                        password = password.Substring(0, (password.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+            Console.WriteLine();
             Console.WriteLine("Perserit fjalekalimin:");
-            string confpassword = Console.ReadLine();
+            string confpassword = "";
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    confpassword += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && confpassword.Length > 0)
+                    {
+                        confpassword = confpassword.Substring(0, (confpassword.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+            Console.WriteLine();
             
             string publicKeyFile = "keys/" + name + ".pub.xml";
             string privateKeyFile= "keys/" + name + ".xml";
