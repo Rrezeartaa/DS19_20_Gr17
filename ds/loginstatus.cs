3132426adbd6792685a355946a225cf2dece2d6d
Rrezeartaa <56273238+Rrezeartaa@users.noreply.github.com>
2020-06-04 16:10:17 +0200

    Klasa per komandat login dhe status

---
+++ ds/loginstatus.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using java.security;
+using System.Reflection.Metadata;
+using Aspose.Words;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Data;
+using System.Security.Cryptography;
+namespace ds
+{
+    class loginstatus
+    {
+        public static void loginGenerateToken(string name)
+        {
+        
+        
+        }
+     }
+ }
